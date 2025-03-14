2613f9f8c0a37ad585e292436d6d16f4bd14a7ac
Miriam Raimondi <miriam.raimondi@edu.unife.it>
2025-03-14 23:20:29 +0100

    Add files via upload

---
+++ Esercitazione1/text_inspector.py
@@ -0,0 +1,11 @@
+def clean_book(source_filename, destination_filename):
+    """
+    Remove everything except the book itself. Return the number of lines of the cleaned book
+
+    source_filename : where to find the book to be cleaned
+    destination_filename : where to store the cleaned book
+
+    >>> clean_book("data/stevenson.txt", "data/stevenson_clean.txt")
+    2530
+    """
+    return 0
